e28e1c0dd3fac2b5e4a355037bbad1716ba2d0a6
Alex Wiltschko <alex.bw@gmail.com>
2015-04-18 12:19:47 -0400

    Working cwrap and paths install.

--- luarocks/build.sh
+++ luarocks/build.sh
@@ -2,12 +2,11 @@
 # - Should use Lua version to set luarocks location. e.g.: lib/luarocks -> /lib/luajit2.0/luarocks
 # - Should set rocks-tree to be "user" instead of "system". Vibes better with the conda approach.
 
-
 ./configure --prefix=$PREFIX \
 			--sysconfdir=$PREFIX/share/lua/ \
-			--force-config \
 			--with-lua-include=$PREFIX/include/luajit-2.0
 			# --rocks-tree=$PREFIX \
+			# --force-config \
 			# --with-lua=$PREFIX \
 			# --lua-version=5.1 \
 			# --versioned-rocks-dir
--- paths/build.sh
+++ paths/build.sh
@@ -1,1 +1,15 @@
+echo $PREFIX
+luarocks list
 luarocks install $(find . -iname '*.rockspec' -d 2 | head -n 1)
+luarocks list
+# cmake -E make_directory build 
+# cd build
+# cmake .. -DCMAKE_BUILD_TYPE=Release \
+# 		-DLUALIB=$(LUALIB) \
+# 		-DLUA_INCDIR="$(LUA_INCDIR)" \
+# 		-DLUA_LIBDIR="$(LUA_LIBDIR)" \
+# 		-DLUADIR="$(LUADIR)" \
+# 		-DLIBDIR="$(LIBDIR)" \
+# 		-DCMAKE_INSTALL_PREFIX="$(PREFIX)" \
+# &&
+# $(MAKE)
