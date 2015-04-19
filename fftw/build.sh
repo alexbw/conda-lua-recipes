2d3df39526e720f2363f3d2c2b46cbbb58ebba53
Alex Wiltschko <alex.bw@gmail.com>
2015-04-19 12:20:39 -0400

    Small typo

--- fftw/build.sh
+++ fftw/build.sh
@@ -1,8 +1,8 @@
-# Install double precision
+# Install single precision
 ./configure --prefix=$PREFIX --exec-prefix=$PREFIX --enable-shared --disable-static --enable-single
 make
 make install
-# Install single precision
+# Install double precision
 ./configure --prefix=$PREFIX --exec-prefix=$PREFIX --enable-shared --disable-static
 make
 make install
