c0089d5641bc23ed5b52bdeb45dddd6d10a521c1
Alex Wiltschko <alex.bw@gmail.com>
2015-12-29 19:02:48 -0500

    Working luajit and luarocks install

--- luarocks/post-link.sh
+++ luarocks/post-link.sh
@@ -1,12 +1,12 @@
 # remove the soft-link to versioned luajit
 # replace it with a small script
 
-LUA="$PREFIX/bin/luajit"
-VERSIONED_LUA="$PREFIX/bin/$(readlink $LUA)"
+# LUA="$PREFIX/bin/luajit"
+# VERSIONED_LUA="$PREFIX/bin/$(readlink $LUA)"
 
-rm -rf $PREFIX/bin/luajit
+# rm -rf $PREFIX/bin/luajit
 
-echo "#!/bin/bash" > $PREFIX/bin/luajit
-echo "" >> $PREFIX/bin/luajit
-echo "LUA_PATH=\"$($PREFIX/bin/luarocks path --lr-path)\" LUA_CPATH=\"$($PREFIX/bin/luarocks path --lr-cpath)\" exec $VERSIONED_LUA \"\$@\"" >> $PREFIX/bin/luajit
-chmod +x $PREFIX/bin/luajit
+# echo "#!/bin/bash" > $PREFIX/bin/luajit
+# echo "" >> $PREFIX/bin/luajit
+# echo "LUA_PATH=\"$($PREFIX/bin/luarocks path --lr-path)\" LUA_CPATH=\"$($PREFIX/bin/luarocks path --lr-cpath)\" exec $VERSIONED_LUA \"\$@\"" >> $PREFIX/bin/luajit
+# chmod +x $PREFIX/bin/luajit
