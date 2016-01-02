d95f9d2a5c2a1b27359cab40609e382f000554d1
Alex Wiltschko <alex.bw@gmail.com>
2016-01-01 19:04:58 -0500

    Lua science metapackage, and audio now compatible with 5.2

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,11 @@
 # Issues:
-# - lua-luaffi doesn't build on 5.3
-# - lua-sundown isn't compatible with 5.1 (goto keywords used)
+# - lua-luaffi doesn't build on 5.3 (https://github.com/facebook/luaffifb/issues/5)
+# - lua-sundown isn't compatible with 5.1 (goto keywords used, https://github.com/torch/sundown-ffi/issues/5)
 # - lua-crypto isn't compatible with 5.3 (there is a fork, see lua-crypto/build.sh)
-# - lua-paths isn't compatible with 5.3
-# - lua-audio isn't compatible with 5.1 and 5.2, not sure why, some luaL_register error
-# - lua-hdf5 isn't compatible with vanilla Lua, even with luabitopt and luaffi. Not sure why.
-# - lua-cunn isn't working with LuaJIT, I don't know why
+# - lua-paths isn't compatible with 5.3 (https://github.com/torch/paths/issues/18)
+# - lua-hdf5 doesn't work with PUC Lua b/c of a bug in luaffi (https://github.com/facebook/luaffifb/issues/4)
+# - lua-cunn isn't working with LuaJIT, I don't know why (https://github.com/torch/cunn/issues/184)
+# - lua-sdl2 only works with LuaJIT, not sure why.
 
 conda clean --index-cache --packages --source-cache
 conda build lua-5.1.5
@@ -18,7 +18,7 @@ conda build sdl2
 conda build luarocks --lua="all"
 conda build lua-luafilesystem --lua="all"
 conda build lua-penlight --lua="all"
-conda build lua-luaffifb --lua=5.1 --lua=5.2 # ffi doesn't work with 5.3. all packages that depend on it won't build for 5.3
+conda build lua-luaffifb --lua=5.1 --lua=5.2
 conda build lua-argcheck --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-cjson --lua="all"
 conda build lua-uuid --lua="all"
@@ -26,10 +26,10 @@ conda build lua-lbase64 --lua="all"
 conda build lua-llthreads2 --lua="all"
 conda build lua-lzmq --lua="all"
 conda build lua-fftw3 --lua=5.1 --lua=5.2 --lua=2.0
-conda build lua-sdl2 --lua=2.0 # currently only supported by luajit
+conda build lua-sdl2 --lua=2.0 # currently only supported by luajit, related to luaffi bug?
 conda build lua-sundown --lua=5.2 --lua=2.0
 conda build lua-class --lua=5.2 --lua=2.0
-conda build lua-crypto --lua=5.2 --lua=5.1 --lua=2.0 # TODO: there is a fork that is compatible with 5.3
+conda build lua-crypto --lua=5.2 --lua=5.1 --lua=2.0
 conda build lua-cwrap --lua="all"
 conda build lua-dok --lua=5.2 --lua=2.0
 conda build lua-paths --lua=5.1 --lua=5.2 --lua=2.0
@@ -37,11 +37,11 @@ conda build lua-torch --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-sys --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-xlua --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-trepl --lua=5.1 --lua=5.2 --lua=2.0
-conda build lua-audio --lua=2.0
+conda build lua-audio --lua=5.2 --lua=2.0
 conda build lua-gnuplot --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-env --lua=5.2 --lua=2.0
 conda build lua-graph --lua=5.1 --lua=5.2 --lua=2.0
-conda build lua-threads --lua=2.0
+conda build lua-threads --lua=2.0 # b/c of sdl2
 conda build lua-image --lua=5.2 --lua=2.0
 conda build lua-cephes --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-randomkit --lua=5.1 --lua=5.2 --lua=2.0
@@ -50,7 +50,7 @@ conda build lua-nn --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-nngraph --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-totem --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-luabitop --lua=5.1 --lua=5.2
-conda build lua-hdf5 --lua=2.0
+conda build lua-hdf5 --lua=2.0 # b/c of luaffi bug
 conda build lua-optim --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-pprint --lua=5.1 --lua=5.2 --lua=2.0
 conda build lua-signal --lua=5.1 --lua=5.2 --lua=2.0
