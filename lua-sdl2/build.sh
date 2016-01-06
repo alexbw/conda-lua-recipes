93c798dda30a6d5d41f17f6d847224999be47551
Alex Wiltschko <awiltschko@twitter.com>
2016-01-06 15:45:45 -0500

    LuaFFI building on lua 5.3

--- lua-sdl2/build.sh
+++ lua-sdl2/build.sh
@@ -8,8 +8,9 @@ $PREFIX/bin/luarocks-admin make_manifest --local-tree
 # they tend to be top-level or in a rocks/ directory.
 # NOTE: we're just picking the first rock we find. If there's
 # more than one, specify it manually.
-ROCK=rocks/sdl2-scm-1.rockspec
-$PREFIX/bin/luarocks install "$ROCK" --local-tree
+luarocks install https://raw.github.com/torch/sdl2-ffi/master/rocks/sdl2-scm-1.rockspec
+# ROCK=rocks/sdl2-scm-1.rockspec
+# $PREFIX/bin/luarocks install "$ROCK" --local-tree
 
 # Add more build steps here, if they are necessary.
 
