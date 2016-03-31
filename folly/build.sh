7b33c109f5a37deee2e8fee980e91e5bfe8b02e0
Alex Wiltschko <alex.bw@gmail.com>
2016-03-31 16:00:57 +0200

    Updates

--- folly/build.sh
+++ folly/build.sh
@@ -1,10 +1,8 @@
-# open $PREFIX
 mv folly/* .
-autoreconf -i
-echo $PREFIX
-echo $(which ld)
-ls $PREFIX/bin
-CXXFLAGS=-I$PREFIX/include:$CXXFLAGS ./configure LDFLAGS=-L$PREFIX/lib # CXXFLAGS=-I$PREFIX/include
+set CC=gcc-4.9
+set CXX=g++-4.9
+autoreconf -ivf
+./configure LDFLAGS="-L$PREFIX/lib" CPPFLAGS="-I$PREFIX/include"
 LD_LIBRARY_PATH=$PREFIX/ make
 make check
 make install
--- gflags/build.sh
+++ gflags/build.sh
@@ -1,4 +1,4 @@
-mkdir build
+mkdir -p build
 cd build
 $PREFIX/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
