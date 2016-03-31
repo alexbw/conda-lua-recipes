f93edd58fe4c9a5197919a0b0e0aaf974a55c637
Alex Wiltschko <alex.bw@gmail.com>
2016-03-31 16:16:54 +0200

    Changes to gflags and glog

--- folly/build.sh
+++ folly/build.sh
@@ -1,6 +1,4 @@
 mv folly/* .
-set CC=gcc-4.9
-set CXX=g++-4.9
 autoreconf -ivf
 ./configure LDFLAGS="-L$PREFIX/lib" CPPFLAGS="-I$PREFIX/include"
 LD_LIBRARY_PATH=$PREFIX/ make
--- gflags/build.sh
+++ gflags/build.sh
@@ -1,5 +1,5 @@
-mkdir -p build
-cd build
+mkdir -p build_
+cd build_
 $PREFIX/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
 make install
